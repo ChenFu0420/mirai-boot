ec449c5295743a684c4fce4149dfc7875bf9098a
ColorfulGhost <admin@iacg.moe>
2021-01-12 17:33:39 +0800

    [A]add nacos

--- src/test/java/moe/iacg/miraiboot/MiraiBootApplicationTests.java
+++ src/test/java/moe/iacg/miraiboot/MiraiBootApplicationTests.java
@@ -1,13 +1,18 @@
 package moe.iacg.miraiboot;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MiraiBootApplicationTests {
 
+	@NacosValue(value = "${mysql.url}")
+	private String url;
+
 	@Test
 	void contextLoads() {
+		System.out.println(url);
 	}
 
 }
