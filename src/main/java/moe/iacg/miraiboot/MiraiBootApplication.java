1e863bed2ea9bb5a5255a05fa08a0e612b1b329b
ColorfulGhost <admin@iacg.moe>
2021-01-12 15:28:04 +0800

    first commit

---
+++ src/main/java/moe/iacg/miraiboot/MiraiBootApplication.java
@@ -0,0 +1,15 @@
+package moe.iacg.miraiboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@SpringBootApplication
+public class MiraiBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraiBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moe/iacg/miraiboot/MiraiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.iacg.miraiboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiraiBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
