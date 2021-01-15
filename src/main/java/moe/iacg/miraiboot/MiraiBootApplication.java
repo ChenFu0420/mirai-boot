8e42204d319bf3bdb1bdf94b5d713de9f2c68e51
ColorfulGhost <admin@iacg.moe>
2021-01-15 09:10:53 +0800

    [E]nacos 调试完成

--- src/main/java/moe/iacg/miraiboot/MiraiBootApplication.java
+++ src/main/java/moe/iacg/miraiboot/MiraiBootApplication.java
@@ -1,17 +1,13 @@
 package moe.iacg.miraiboot;
 
 
-import com.alibaba.nacos.api.config.ConfigType;
-import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
-//@EnableNacosConfig(readConfigTypeFromDataId = false)
-@NacosPropertySource(dataId = "mirai-boot-client.properties", autoRefreshed = true,type = ConfigType.PROPERTIES)
 public class MiraiBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(MiraiBootApplication.class, args);
