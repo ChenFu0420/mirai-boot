29682e72d235cebe5de8af68228f5f52caf0e245
ColorfulGhost <admin@iacg.moe>
2021-01-26 22:46:30 +0800

    [A]telegram bot update

--- src/main/java/moe/iacg/miraiboot/telegram/AE86QQBot.java
+++ src/main/java/moe/iacg/miraiboot/telegram/AE86QQBot.java
@@ -1,6 +1,5 @@
 package moe.iacg.miraiboot.telegram;
 
-import cn.hutool.core.io.FileUtil;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +11,7 @@ import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.stickers.Sticker;
+import org.telegram.telegrambots.meta.api.objects.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,10 +35,8 @@ public class AE86QQBot extends TelegramLongPollingBot {
     private BotUtils botUtils;
 
 
-
-
     AE86QQBot() {
-        super(defaultBotOptions());
+//        super(defaultBotOptions());
     }
 
     private static DefaultBotOptions defaultBotOptions() {
@@ -79,20 +73,33 @@ public class AE86QQBot extends TelegramLongPollingBot {
             for (var tgGroupIdByGroupId : tgGroupIdByGroupId().entrySet()) {
 
                 Msg msg = Msg.builder();
+                User user = updateMessage.getFrom();
+                msg.text(user.getFirstName()).text(" ").text(user.getLastName()).text("：");
+
                 if (updateMessage.getChatId().equals(tgGroupIdByGroupId.getKey())) {
                     if (updateMessage.hasSticker()) {
                         String fileId = updateMessage.getSticker().getFileId();
                         GetFile getFile = new GetFile();
                         getFile.setFileId(fileId);
-                        FileUtil.file( execute(getFile).getFilePath());
-//                        sendSticker.getSticker();
-                        botUtils.sendGroupMsg(tgGroupIdByGroupId.getValue(),msg);
+                        msg.image(execute(getFile).getFileUrl(getBotToken()));
+                        botUtils.sendGroupMsg(tgGroupIdByGroupId.getValue(), msg);
                     }
                     if (updateMessage.hasText()) {
+                        msg.text(updateMessage.getText());
+                        botUtils.sendGroupMsg(tgGroupIdByGroupId.getValue(), msg);
 
                     }
                     if (updateMessage.hasPhoto()) {
 
+                        for (PhotoSize photoSize : updateMessage.getPhoto()) {
+                            GetFile getFile = new GetFile();
+                            getFile.setFileId(photoSize.getFileId());
+                            File execute = execute(getFile);
+
+                            String fileUrl = execute.getFileUrl(getBotToken());
+                            msg.image(fileUrl);
+                        }
+                        botUtils.sendGroupMsg(tgGroupIdByGroupId.getValue(), msg);
                     }
                     if (updateMessage.hasAnimation()) {
 
